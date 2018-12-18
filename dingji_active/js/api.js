d8748c5dfd3fed0e73e6172800c33b184c3139b8
VIPsth <VIPsongtaohong@yeah.net>
2018-12-18 17:11:17 +0800

    add

--- dingji_active/js/api.js
+++ dingji_active/js/api.js
@@ -1,442 +1,249 @@
 // 发送验证码
-let protect_sent_msg = "/api/protect/sent_msg";
+let activity_sent_msg = "/api/activity/send_msg";
 // 登录
-let protect_login = "/api/protect/login";
-// 查询推荐码
-let protect_get_code = "/api/protect/get_code";
-// 设置推荐码
-let protect_set_code = "/api/protect/set_code";
-// 查询学员是否已经被保护
-let protect_select_student = "/api/protect/select_student";
-// 意向资源添加保护
-let protect_resource_add = "/api/protect/resource_add";
-// 资源保护列表
-let protect_resource_list = "/api/protect/resource_list";
-// 成交学员详细列表
-let protect_deal_list = "/api/protect/deal_list";
-// 团队成交数据
-let protect_deal_team = "/api/protect/deal_team";
-// 开发记录列表
-let protect_develop_list = "/api/protect/develop_list";
-// 开发记录查询
-let protect_develop_show = "/api/protect/develop_show";
-// 开发记录编辑
-let protect_develop_edit = "/api/protect/develop_edit";
-// 资源保护人员替换
-let protect_resource_replace = "/api/protect/resource_replace";
-// 添加开发记录
-let protect_develop_add = "/api/protect/develop_add";
-// 解除资源保护
-let protect_resource_remove = "/api/protect/resource_remove";
+let activity_login = "/api/activity/login";
+// 个人信息
+let activity_info = "/api/activity/info";
+// 根据邀请人id获取电话号码
+let activity_get_tel = "/api/activity/get_tel";
+// 预存
+let activity_prestore = "/api/activity/prestore";
+// 分享前
+let activity_share = "/api/activity/share";
+// 邀请列表
+let activity_invite_list = "/api/activity/invite_list";
+// 邀请的时候调一下
+let activity_invite = "/api/activity/invite";
+// 抽奖
+let activity_luck = "/api/activity/luck";
+// 判断是否可以抽奖
+let activity_chance = "/api/activity/chance";
 
+// 根据邀请人id获取电话号码
+function get_tel() {
+	let ajaxdata = {
+		id: getQueryString("yaoqing_id")
+	}
+	let data = ajaxPost(activity_get_tel, ajaxdata);
+	if (data.status == "200") {
+		$(".open-popo").css("display","flex");
+		$(".phone").html(data.data.tel)
+	}
+}
 // 发送验证码
 function sent_msg(tel) {
 	let ajaxdata = {
 		tel: tel
 	}
-	let data = ajaxPost(protect_sent_msg, ajaxdata);
+	let data = ajaxPost(activity_sent_msg, ajaxdata);
 	if (data.returnstatus == "Success") {
-		mui.alert("验证码已发送，请注意查收", " ")
+		alert("验证码已发送，请注意查收", " ")
 		$(".codebtn").css("pointer-events", "none");
 		$(".codebtn").text(60);
 		num();
-		$(".codebtn").addClass("ztsfontcolor").removeClass("ztsbuttom");
+		// $(".codebtn").addClass("ztsfontcolor").removeClass("ztsbuttom");
 	} else {
 
-		mui.alert(data.message, " ")
+		alert(data.message, " ")
 	}
 }
 // 登录
 function login(ajaxdata) {
-	let data = ajaxPost(protect_login, ajaxdata);
+	let data = ajaxPost(activity_login, ajaxdata);
 	if (data.status == "200") {
 		localStorage.setItem("userInfo", JSON.stringify(data.data))
-		location.href = "index.html";
+		alert("登录成功");
+		$(".iframe-popo").hide(1);
+		$(".total_amount").html(data.data.total_amount)
+		window.location.reload();
 	} else {
-		mui.alert(data.msg, " ")
+		alert(data.msg, " ")
 	}
 }
-// 查询推荐码
-function get_code(tel) {
-	let ajaxdata = {
-		tel: tel
+// 判断是否登录
+function toLogin() {
+	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+	if (userInfo == null || userInfo == "" || userInfo == "null" || userInfo == undefined || userInfo == "undefined") {
+		$(".iframe-popo").show();
 	}
-	let data = ajaxPost(protect_get_code, ajaxdata);
+}
+
+// 个人信息
+function info() {
+	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+	let ajaxdata = {
+		tel: userInfo.tel,
+	}
+	let data = ajaxPost(activity_info, ajaxdata);
+	if (data.status == "200") {
+		localStorage.setItem("userInfo", JSON.stringify(data.data))
+		$(".total_amount").html(data.data.total_amount)
+		if (data.data.is_prestore == 1) {
+			$(".ycmoney").html("300");
+		} else {
+			$(".ycmoney").html("0");
+		}
+		if (data.data.is_share == 0) {
+			$(".fxmoney").html(0)
+		} else {
+			$(".fxmoney").html(100)
+
+		}
+		// 
+	}
+}
+// 邀请列表
+function invite_list() {
+	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+	let ajaxdata = {
+		tel: userInfo.tel,
+	}
+	let data = ajaxPost(activity_invite_list, ajaxdata);
+	if (data.status == "200") {
+		let src = "";
+		for (var i = 0; i < data.data.length; i++) {
+			src += '<div class="row">' + data.data[i].name + ' ' + data.data[i].tel + '</div>';
+		}
+		$(".yqnum").html(data.data.length)
+		$(".yqmoney").html(data.data.length * 100)
+		$(".row-box").html(src);
+	}
+}
+// 邀请
+function invite() {
+	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+	let ajaxdata = {
+		tel: userInfo.tel,
+	}
+	let data = ajaxPost(activity_invite, ajaxdata);
+}
+// 预存
+function prestore() {
+	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+	let yaoqing_id = getQueryString("yaoqing_id"),
+		id = "";
+	if (yaoqing_id != null || yaoqing_id != "" || yaoqing_id != "null" || yaoqing_id != undefined || yaoqing_id !=
+		"undefined") {
+		id = yaoqing_id;
+	} else {
+		id = getQueryString("id");
+
+	}
+	let ajaxdata = {
+		amount: 100,
+		tel: userInfo.tel,
+		id: getQueryString("id")
+	}
+	let data = ajaxPost(activity_prestore, ajaxdata);
+	if (data.status == "200") {
+		alert("预存成功");
+		info();
+	} else {
+		alert(data.msg)
+	}
+}
+// 分享前
+function share() {
+	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+	let ajaxdata = {
+		tel: userInfo.tel,
+	}
+	let data = ajaxPost(activity_share, ajaxdata);
+	if (data.status == "200") {
+		do_share();
+
+	}
+}
+// 抽奖
+function luck() {
+	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+	let ajaxdata = {
+		tel: userInfo.tel,
+	}
+	let data = ajaxPost(activity_luck, ajaxdata);
+	if (data.status == "200") {
+		var random = Math.floor(Math.random() * 360); //生成随机数  
+		$("#turntable").rotate({
+			duration: 3000,
+			//转动时间间隔（速度单位ms）  
+			angle: 0,
+			//指针开始角度  
+			animateTo: 3600 + parseInt(data.data.angle),
+			//转动角度，当转动角度到达3600+random度时停止转动。 
+			easing: $.easing.easeOutSine,
+			//easing动画效果 
+			callback: function() { //回调函数  
+				alert(data.data.prize);
+			}
+		});
+	}
+}
+// 查看是否可以抽奖
+function chance() {
+
+	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+	let ajaxdata = {
+		id: userInfo.id,
+	}
+	let data = ajaxPost(activity_chance, ajaxdata);
 	return data;
 }
-// 设置推荐码
-function set_code(ajaxdata) {
 
-	let data = ajaxPost(protect_set_code, ajaxdata);
+//分享弹出页面层
+function do_share() {
+	layer.open({
+		type: 1,
+		content: $('#share_box').html(),
+		anim: 'up',
+		style: 'position:fixed; bottom:0; left:0; width: 100%; padding:10px 0; border:none;'
+	});
+	alert("分享后需要小伙伴浏览您的分享链接才可以抽奖哟！")
+}
+//邀请弹出页面层
+function do_Invitation() {
 
-	mui.alert(data.msg, " ")
-	if (data.status == "200") {
-		$(".setCode").removeClass("ztsbuttom");
-		$(".setCode").css("pointer-events", "none");
-		$("#recomcode").attr("disabled", true);
-		$(".updetaCode").text("修改推荐码");
-		$(".updetaCode").css("width", ".65rem");
-	}
+
+	layer.open({
+		type: 1,
+		content: $('#Invitation_box').html(),
+		anim: 'up',
+		style: 'position:fixed; bottom:0; left:0; width: 100%; padding:10px 0; border:none;'
+	});
 }
 
-// 查询学员是否已经被保护
-function select_student(ajaxdata) {
-	let data = ajaxPost(protect_select_student, ajaxdata);
-	if (data.status == "200") {
-		if (data.data.num == 0) {
-			mui.confirm("您的保护名额已经用完您可以替换掉无法成交的学员", " ", ['不用了', '去替换'], function(e) {
-				if (e.index == 1) {
-					$(".index-TstudentDetail").css("display", "block");
-				}
-			})
-		} else {
+function openApp() {
+	// 安卓：
 
-			mui.confirm("您查询的学员未保护，是否保护？您当前还可以添加 " + data.data.num + " 名", " ", ['取消', '添加保护'], function(e) {
-				if (e.index == 1) {
-					resource_add(ajaxdata);
-				}
-			})
+	var state = null;
+	try {
+		if (scheme != '') {
+			openIframe.src = '[scheme]://[host]/[openwith]';
 		}
-	}
-	if (data.status == "500") {
-
-		mui.alert("您查询的学员已经被保护\n\n保护时间：<font>" + data.data.protect_time + "</font>\n脱保时间：<font>" + data.data.protect_time +
-			"</font>", " ")
-	}
-}
-// 意向资源添加保护
-function resource_add(ajaxdata) {
-
-	let data = ajaxPost(protect_resource_add, ajaxdata);
-	if (data.status == "200") {
-		mui.alert("添加成功", " ", function() {
-
-			$(".mui-popup").remove()
-		})
-	}
-}
-
-// 资源保护列表
-function resource_list(type) {
-	let jcth = "";
-	if (type == "popo") {
-		jcth = "替换";
-	}
-	if (type == "page") {
-		jcth = "解除";
-	}
-	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-	let ajaxdata = {
-		person: userInfo.phone
-	}
-	let data = ajaxPost(protect_resource_list, ajaxdata);
-	let rlData = data.data.list;
-	let deactivationData = data.data.deactivation;
-	// 资源保护列表
-	let listSrc = "";
-	// 已脱保列表
-	let deactivationSrc = "";
-	let src = "";
-	let numStudent = 0;
-	for (var i = 0; i < rlData.length; i++) {
-		let time = rlData[i].time;
-		let valuesrc = "";
-		let titlesrc = "";
-		let dxSrc = "";
-		let daynumStudent = 0;
-		if (i == 0) {
-			titlesrc = '<div class="title">' +
-				'您共成交<span class="ztsfontcolor numStudent">28</span>名学员' +
-				'</div>';
-		}
-		if (i == (rlData.length - 1)) {
-			dxSrc = '<div class="dx">人家有底线啦</div>';
-		}
-		delete rlData[i]['time'];
-		$.each(rlData[i], function(index, value) {
-			valuesrc += '<div class="item ">' +
-				'<div class="name">' + value.name + '</div>' +
-				'<div class="tel">' + value.tel + '</div>' +
-				'<div class="relieve"><span class="time">' + value.deactivation_time.substring(0, value.deactivation_time.length -
-					3) + '</span><span class="line">|</span><span class="replace" id="' + value.id + '">' + jcth +
-				'<span class="mui-icon mui-icon-forward"></span></span></div>' +
-				'</div>';
-			numStudent++;
-			daynumStudent++;
-		});
-		listSrc = '<div class="column box-shadow">' +
-			titlesrc +
-			'<div class="item ztsfontcolor">' +
-			'<span><span class="time">' + time + '</span><span class="text6">共' + daynumStudent + '名</span></span>' +
-			'</div>' +
-			valuesrc +
-			dxSrc +
-			'</div>';
-	}
-
-
-	$(".TstudentDetail .content").html(listSrc);
-	$(".numStudent").text(numStudent);
-	if (type == "page") {
-		let srcda = "";
-		for (var i = 0; i < deactivationData.length; i++) {
-			srcda += '<div class="item ">' +
-				'<div>' + deactivationData[i].name + '</div>' +
-				'<div>' + deactivationData[i].tel + '</div>' +
-				'<div class="relieve deinsurance"><span class="time">' + deactivationData[i].deal_time +
-				'</span><span class="line">|</span>' +
-				deactivationData[i].status + '</div>' +
-				'</div>';
-		}
-
-		deactivationSrc = '<div class="column box-shadow">' +
-			'<div class="item ztsfontcolor">' +
-			'已脱保名单' +
-			'</div>' +
-			srcda +
-			'<div class="dx">人家有底线啦</div>' +
-			'</div>';
-		$(".TstudentDetail .content").append(deactivationSrc);
-		$(".TstudentDetail .content .column").eq($(".TstudentDetail .content .column").length - 2).find(".dx").remove();
-
-	}
-}
-
-// 资源保护人员替换
-function resource_replace(id) {
-
-	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-	let ajaxdata = {
-		person: userInfo.phone,
-		id: id,
-		tel: $("#studentPhone").val(),
-		name: $("#studentName").val()
-	}
-	let data = ajaxPost(protect_resource_replace, ajaxdata);
-	if (data.status == "200") {
-		mui.alert("替换保护成员成功", " ");
-	}
-	console.log(data)
-}
-
-// 解除资源保护
-function resource_remove(id) {
-	
-	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-	let ajaxdata = {
-		person: userInfo.phone,
-		id: id,
-	}
-	let data = ajaxPost(protect_resource_remove, ajaxdata);
-	if (data.status == "200") {
-		mui.alert("解除保护成功", " ", function() {
-			window.location.reload();
-		})
-	}
-
-	console.log(data)
-}
-// 成交学员详细列表
-function deal_list() {
-
-	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-	let ajaxdata = {
-		person: userInfo.phone
-	}
-	let data = ajaxPost(protect_deal_list, ajaxdata);
-	let src = "";
-	let numStudent = 0;
-	for (var i = 0; i < data.data.length; i++) {
-
-		let time = data.data[i].time;
-		let valuesrc = "";
-		let titlesrc = "";
-		let dxSrc = "";
-		let daynumStudent = 0;
-		if (i == 0) {
-			titlesrc = '<div class="title">' +
-				'您共成交<span class="ztsfontcolor numStudent">28</span>名学员' +
-				'</div>';
-		}
-		if (i == (data.data.length - 1)) {
-			dxSrc = '<div class="dx">人家有底线啦</div>';
-		}
-		delete data.data[i]['time'];
-		$.each(data.data[i], function(index, value) {
-			valuesrc += '<div class="item ">' +
-				'<div>' + value.name + '</div>' +
-				'<div>' + value.tel + '</div>' +
-				'<div>' + value.school_name + '</div>' +
-				'</div>';
-			numStudent++;
-			daynumStudent++;
-		});
-		src += '<div class="column box-shadow">' +
-			titlesrc +
-			'<div class="item ztsfontcolor">' +
-			'<span><span class="time">' + time + '</span><span class="text6">共' + daynumStudent + '名</span></span>' +
-			'</div>' +
-			valuesrc +
-			dxSrc +
-			'</div>';
-	}
-	$(".item1").html(src)
-	$(".numStudent").text(numStudent);
-}
-
-function deal_team() {
-
-
-	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-	let ajaxdata = {
-		person: userInfo.phone
-	}
-	let data = ajaxPost(protect_deal_team, ajaxdata);
-		let src = "";
-		let numStudent = 0;
-		for (var i = 0; i < data.data.length; i++) {
-	
-			let time = data.data[i].time;
-			let valuesrc = "";
-			let titlesrc = "";
-			let dxSrc = "";
-			let daynumStudent = 0;
-			if (i == 0) {
-				titlesrc = '<div class="title">' +
-					'您的团队共成交<span class="ztsfontcolor numStudent">28</span>名学员' +
-					'</div>';
-			}
-			if (i == (data.data.length - 1)) {
-				dxSrc = '<div class="dx">人家有底线啦</div>';
-			}
-			delete data.data[i]['time'];
-			$.each(data.data[i], function(index, value) {
-				let personnumStudent = 0;
-
-				let person=value['person'];
-			delete value['person'];
-			let value1src="";
-			$.each(value,function(index1,value1){
-				 value1src+= '<div class="item ">' +
-					'<div>' + value1.name + '</div>' +
-					'<div>' + value1.tel + '</div>' +
-					'<div>' + value1.school_name + '</div>' +
-					'</div>';
-					
-					numStudent++;
-					daynumStudent++;
-					personnumStudent++;
-			})
-				valuesrc+=
-				'<div class="item fu-item ztsfontcolor">'+
-					'<span><span class="time">'+person+'&nbsp;&nbsp;&nbsp;&nbsp;</span><span class="text6">共'+personnumStudent+'名</span></span>'+
-				'</div>'+value1src;
-			});
-			src += '<div class="column box-shadow">' +
-				titlesrc +
-				'<div class="item ztsfontcolor">'+
-					'<div class="fu-title">'+
-					'<span><span class="time">' + time + '</span><span class="text6">共' + daynumStudent + '名</span></span>' +
-						'</div>'+
-				'</div>'+
-				valuesrc +
-				dxSrc +
-				'</div>';
-		}
-		$(".item2").html(src)
-		$(".numStudent").text(numStudent);
-}
-// 开发记录列表
-function develop_list() {
-
-	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-	let ajaxdata = {
-		person: userInfo.phone
-	}
-	let data = ajaxPost(protect_develop_list, ajaxdata);
-	console.log(data)
-
-	let src = "";
-	let numStudent = 0;
-	for (var i = 0; i < data.data.length; i++) {
-
-		let time = data.data[i].time;
-		let valuesrc = "";
-		let titlesrc = "";
-		let dxSrc = "";
-		let daynumStudent = 0;
-		if (i == 0) {
-			titlesrc = '<div class="title">' +
-				'共 <span class="ztsfontcolor numStudent">28</span> 名保护成员' +
-				'</div>';
-		}
-		if (i == (data.data.length - 1)) {
-			dxSrc = '<div class="dx">人家有底线啦</div>';
-		}
-		delete data.data[i]['time'];
-		$.each(data.data[i], function(index, value) {
-			valuesrc += '<div class="item ">' +
-				'<div>' + value.name + '</div>' +
-				'<div>' + value.tel + '</div>' +
-				'<div class="edit" id=' + value.id + '>编辑<span class="mui-icon mui-icon-arrowright"></span></div>' +
-				'</div>';
-			numStudent++;
-			daynumStudent++;
-		});
-		src += '<div class="column box-shadow">' +
-			titlesrc +
-			'<div class="item ztsfontcolor">' +
-			'<span><span class="time">' + time + '</span><span class="text6">共' + daynumStudent + '名</span></span>' +
-			'</div>' +
-			valuesrc +
-			dxSrc +
-			'</div>';
-	}
-	$(".content").html(src)
-	$(".numStudent").text(numStudent);
-}
-
-// 添加开发记录
-function develop_add(ajaxdata) {
-
-	let data = ajaxPost(protect_develop_add, ajaxdata);
-	if (data.status == "200") {
-		mui.alert("添加成功", " ");
+	} catch (e) {}
+	if (state) {
+		window.close();
 	} else {
-
-		mui.alert(data.msg, " ");
+		location.href = '下载地址';
 	}
-}
-// 开发记录查询
-function develop_show() {
 
-	let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-	let ajaxdata = {
-		id: getQueryString("id"),
-		person: userInfo.phone
-	}
-	let data = ajaxPost(protect_develop_show, ajaxdata);
-	
-	if (data.status == "200") {
-		$("#selName").val(data.data.name); //复制[string] 学员姓名
-		$("#selTel").val(data.data.tel);//[string] 是 学员电话号码
-		$("#selChannel").val(data.data.channel);//[string] 是 资源获取途径
-		$("#selOne").val(data.data.one);//[string] 一次跟进情况
-		$("#selTwo").val(data.data.two);//[string] 两次跟进情况
-		$("#selThree").val(data.data.three);//[string] 三次跟进情况
-		$("#selDeal_time").val(settiem(data.data.deal_time));//[string] 预计成交时间
-		$("#selRemark").val(data.data.remark);
+	// 苹果：
+
+	if (scheme != '') {
+		window.location.href = '[scheme]://';
 	}
 }
 
-// 开发记录编辑
-function develop_edit(ajaxdata){
-	let data = ajaxPost(protect_develop_edit, ajaxdata);
-	if (data.status == "200") {
-			mui.alert("修改成功", " ");
-			history.back();
-		} else {
-	
-			mui.alert(data.msg, " ");
-		}
+function downloadIamge(selector, name) {
+	// 通过选择器获取img元素，
+	var img = document.querySelector(selector)
+	// 将图片的src属性作为URL地址
+	var url = img.src
+	var a = document.createElement('a')
+	var event = new MouseEvent('click')
+
+	a.download = name || '下载图片名称'
+	a.href = url
+
+	a.dispatchEvent(event)
+	alert(1)
 }
